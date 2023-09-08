b72e8cc4ac2240f0f4be7804dd73308123ceb5f0
Emeka Nkuku <cgnkuku@knox.edu>
2023-09-08 15:53:57 -0500

    Updates

---
+++ precache-manifest.bd4141087ead988d713a26e479ba6f6e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c170de5d147d529488e",
+    "url": "/static/css/main.99811bd5.chunk.css"
+  },
+  {
+    "revision": "0c170de5d147d529488e",
+    "url": "/static/js/main.0c170de5.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "a3cf76c944d94482004d",
+    "url": "/static/css/2.659e2693.chunk.css"
+  },
+  {
+    "revision": "a3cf76c944d94482004d",
+    "url": "/static/js/2.a3cf76c9.chunk.js"
+  },
+  {
+    "revision": "4ff977459512dd7b9c00",
+    "url": "/static/js/runtime~main.4ff97745.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3d0ec57f40d62ba6a8e0d1857bb8f7d5",
+    "url": "/static/media/bootstrap-icons.3d0ec57f.woff"
+  },
+  {
+    "revision": "a30fb81bd52143bcd4de2898422ac8b9",
+    "url": "/static/media/bootstrap-icons.a30fb81b.woff2"
+  },
+  {
+    "revision": "559b4d9c6046d945e77a869beef32c26",
+    "url": "/index.html"
+  }
+];
